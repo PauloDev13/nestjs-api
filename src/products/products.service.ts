b28074f1f68ac41261f7951d853a133193150ed8
PauloDev <devpgm2020@gmail.com>
2021-07-01 00:48:14 -0300

    Implementando CRUD de product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityNotFoundError, Repository } from 'typeorm';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productsRepo: Repository<Product>,
+  ) {}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const product = this.productsRepo.create(createProductDto);
+    return this.productsRepo.save(product);
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productsRepo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.productsRepo.findOne(id);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const updateResult = await this.productsRepo.update(id, updateProductDto);
+    if (updateResult.affected) {
+      throw new EntityNotFoundError(Product, id);
+    }
+    return this.productsRepo.findOne(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const deleteResult = await this.productsRepo.delete(id);
+    if (deleteResult.affected) {
+      throw new EntityNotFoundError(Product, id);
+    }
   }
 }
